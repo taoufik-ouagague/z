3583381fe89d146a2a10942aa942fbb95573acb4
Taoufik Ouagague <taoufik.ouagague.08@gmail.com>
2026-02-04 12:50:38 +0100

    Describe what you changed

--- vite.config.ts
+++ vite.config.ts
@@ -4,6 +4,7 @@ import react from '@vitejs/plugin-react';
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
+  base: '/vl/', // This must match your GitHub repo name!
   optimizeDeps: {
     exclude: ['lucide-react'],
   },
